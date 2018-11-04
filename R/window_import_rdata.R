c0ae49d068a5bcca02cd06ea184697aca043c31c
Vilmantas Gegzna <GegznaV@gmail.com>
2018-11-04 03:41:18 +0200

    Update R/window_import_rdata.R

---
+++ R/window_import_rdata.R
@@ -0,0 +1,97 @@
+#' ===========================================================================
+#' TODO:
+#' 1) Add dialogue with warning message if to proceed;
+#' 2) Fix issue related to Relative/Absolute path.
+#'
+#' @rdname Menu-window-functions
+#' @export
+#' @keywords internal
+window_import_rdata <- function() {
+    initial_dir <- getwd()
+
+    file <- tclvalue(tkgetOpenFile(
+        title = "Choose RData File to Import",
+        initialdir  = initial_dir,
+        multiple = FALSE,
+        filetypes = gettext_Bio("{ {R Data Files} {.RData .rda .Rda .RDA .Rdata } } { {All Files} * }")))
+
+    if (file == "") {
+        tkfocus(CommanderWindow())
+        return()
+    }
+
+    # Check names in RData file --------------------------------------------------
+    rdata_names       <- load(file, envir = new.env())
+    current_obj_names <- objects(envir = .GlobalEnv, all.names = TRUE)
+    overwrite_names   <- rdata_names[rdata_names %in% current_obj_names]
+
+
+    if (length(overwrite_names) > 0) {
+        o_names  <- str_c(overwrite_names, collapse = ", ")
+        warn_msg <- str_c(
+            "\nIn you workspace, the following objects will be overwritten: \n",
+            o_names,
+            "\n\nDo you want to proceed?"
+           )
+
+        choice <- tk_messageBox("yesno", warn_msg,
+                                "Do you want to proceed?",
+                                default = "no")
+
+        if (choice == "no") {
+            Message("Data import canceled.", type = "warn")
+            return()
+        }
+      }
+
+    # Cursor ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    setBusyCursor()
+    on.exit(setIdleCursor())
+
+
+    # Change these lines [!!!]: ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    make_relative = TRUE
+
+    if (make_relative) {
+        file <- make_relative_path(file)
+    }
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+    command <- str_glue(
+        '## Import data from RData file\n',
+        'load("{file}")'
+    )
+
+    dsname <- justDoIt(command)
+    logger(command)
+
+    # --------------------------------------------------------------------------
+    # if (class(dsname)[1] != "try-error") {
+    #     Message("...", type = "error")
+    #     return()
+    # }
+
+    if (length(dsname) == 1) {
+        activeDataSet(dsname)
+
+    } else {
+        Message(
+            message = paste(
+                gettextRcmdr("More than one object was imported. Object names:\n"),
+                paste(dsname, collapse = ", ")
+            ),
+            type = "note")
+    }
+
+    if (length(overwrite_names) > 0) {
+        warn_msg2 <- str_c(
+            "\nThe following objects were overwritten: \n",
+            o_names
+        )
+        Message(warn_msg2, type = "warning")
+    }
+
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    tkfocus(CommanderWindow())
+}
