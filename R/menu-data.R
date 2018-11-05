064309288d52a63adc53c0b952e604c2db175751
Vilmantas Gegzna <GegznaV@gmail.com>
2018-11-05 03:22:18 +0200

    command_list_objects added

--- R/menu-data.R
+++ R/menu-data.R
@@ -4,3 +4,27 @@
 #' @keywords internal
 NULL
 
+# ============================================================================
+#' @rdname Menu-window-functions
+#' @export
+#' @keywords internal
+command_list_objects <- function() {
+
+    # Inputs (not implemented) -----------------------------------------------
+    show_hidden <- FALSE
+
+    if (show_hidden) {
+        all_names <- "all.names = TRUE"
+    } else {
+        all_names <- ""
+    }
+
+    # Code -------------------------------------------------------------------
+    command <-  str_glue(
+        "## List objects in R workspace \n",
+        "objects({all_names})"
+        )
+
+    doItAndPrint(command)
+
+}
