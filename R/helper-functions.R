93a37871dbb7ae5af1332b007d46b60c196ff563
Vilmantas Gegzna <GegznaV@gmail.com>
2018-11-06 16:22:07 +0200

    Update R/helper-functions.R

--- R/helper-functions.R
+++ R/helper-functions.R
@@ -407,6 +407,49 @@ is_valid_name <- function(name) {
 
 # + Duplicated name ---------------------------------------------------------------
 
+# TODO:
+#  Ar reikalingos funkcijos `box_ask_to_replace`, `replace_object`,
+#  `replace_variable `
+#
+#
+#  ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ?
+#' Message box, which asks if object should be replaced
+#' @name box_ask_to_replace
+#' @param name The name of the object.
+#' @param type The type of the object.
+#' @keywords internal
+#' @return Logical value: TRUE -- to replace or FALSE -- not to replace.
+# @export
+#
+# @examples
+box_ask_to_replace <- function(name, type = gettextRcmdr("Object")) {
+    rez <- RcmdrTkmessageBox(
+        message = sprintf(
+            gettextRcmdr('%s "%s" already exists.\nOverwrite the %s?'),
+            type,
+            name,
+            tolower(type)
+        ),
+        icon = "warning",
+        type = "yesno",
+        default = "no"
+    )
+    tclvalue(rez) == "yes"
+}
+#  ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ?
+#' @rdname box_ask_to_replace
+replace_object <- function(name, type = gettextRcmdr("Object")) {
+    box_ask_to_replace(name, type)
+}
+#  ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ?
+#' @rdname box_ask_to_replace
+replace_variable <- function(name, type = gettextRcmdr("Variable")) {
+    box_ask_to_replace(name, type)
+}
+#  ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ?
+
+
+
 #' Message box to confirm replacement
 #'
 #' @param name string - name of the object to repace.
@@ -416,8 +459,8 @@ is_valid_name <- function(name) {
 #' @export
 #'
 #' @examples
-#' msg_box_check_replace()
-msg_box_check_replace <- function(name, type = "Variable") {
+#' msg_box_confirm_replacement()
+msg_box_confirm_replacement <- function(name, type = "Variable") {
     Type <- stringr::str_to_title(type)
 
     tclvalue(RcmdrTkmessageBox(
@@ -436,7 +479,7 @@ replace_duplicated_variable <- function(name) {
     #     - variable exists but user agrees to overvrite it.
 
     if (name %in% listVariables()) {
-        msg_box_check_replace(name, "Variable") == "yes"
+        msg_box_confirm_replacement(name, "Variable") == "yes"
     } else {
         TRUE
     }
@@ -449,10 +492,10 @@ replace_duplicated_obj <- function(name, envir = .GlobalEnv) {
     #     - object exists but user agrees to overvrite it.
 
     if (name %in% listDataSets(envir = envir)) {
-        msg_box_check_replace(name, "Dataset") == "yes"
+        msg_box_confirm_replacement(name, "Dataset") == "yes"
 
     } else if (name %in% objects(envir = envir, all.names = TRUE)) {
-        msg_box_check_replace(name, "Object") == "yes"
+        msg_box_confirm_replacement(name, "Object") == "yes"
 
     } else {
         TRUE
