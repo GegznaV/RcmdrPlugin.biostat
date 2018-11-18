78600df314c4b17b905c1dea90fc6e267b36b74c
Vilmantas Gegzna <GegznaV@gmail.com>
2018-11-18 13:52:20 +0200

    Update 01_package.R

--- R/01_package.R
+++ R/01_package.R
@@ -3,15 +3,17 @@
 #'
 #' @title An R Commander Plug-in for Basic Data Analysis Tasks
 #'
-#'
 #' @description
-#'
 #' \code{RcmdrPlugin.biostat} is an \code{Rcmdr} plugin for functions in package
 #' \code{biostat} and other common (bio)statistical routines.
 #'
 #' @importFrom glue glue
 #' @importFrom dplyr glimpse is_grouped_df
 #' @importFrom magrittr "%>%"  "%<>%"  "%T>%"  "%$%"
+#' @importFrom Rcmdr activeDataSet
+#' @importFrom graphics plot
+#' @importFrom stats p.adjust.methods C
+#' @importFrom utils browseURL packageVersion data
 #' @import stringr
 #' @import purrr
 #' @import biostat
@@ -20,10 +22,7 @@
 #' @import RcmdrPlugin.EZR
 #' @import tcltk
 #' @import tcltk2
-#' @importFrom Rcmdr activeDataSet
-#' @importFrom graphics plot
-#' @importFrom stats p.adjust.methods C
-#' @importFrom utils browseURL packageVersion data
+
 
 NULL
 
