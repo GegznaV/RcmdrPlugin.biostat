67a4ed6c5f5434a2f0c69e5a63c8a49e293779d9
Vilmantas Gegzna <GegznaV@gmail.com>
2018-11-22 19:00:58 +0200

    Functions moved

--- R/menu-data.R
+++ R/menu-data.R
@@ -4,3 +4,99 @@
 #' @keywords internal
 NULL
 
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# "Data" menu related functions ==============================================
+
+# Manage dataset -------------------------------------------------------------
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#' @rdname Menu-window-functions
+#' @export
+#' @keywords internal
+command_dataset_refresh <- function() {
+    Rcmdr::activeDataSet(Rcmdr::ActiveDataSet())
+}
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+#' @rdname Menu-window-functions
+#' @export
+#' @keywords internal
+command_dataset_class <- function() {
+    doItAndPrint(str_glue("## The R class of the dataset\n",
+                          "class({ActiveDataSet()})"))
+}
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+#' @rdname Menu-window-functions
+#' @export
+#' @keywords internal
+# List column names
+command_colnames <- function() {
+    doItAndPrint(str_glue("## Column names\n",
+                          "colnames({ActiveDataSet()})"))
+}
+
+
+# ============================================================================
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+#' @rdname Menu-window-functions
+#' @export
+#' @keywords internal
+command_dataset_print <- function() {
+    doItAndPrint(str_glue("print({ActiveDataSet()})"))
+}
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+#' @rdname Menu-window-functions
+#' @export
+#' @keywords internal
+command_dataset_view <- function() {
+    doItAndPrint(str_glue("View({ActiveDataSet()})"))
+}
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+#' @rdname Menu-window-functions
+#' @export
+#' @keywords internal
+command_dataset_dim <- function() {
+    doItAndPrint(str_glue("## Number of rows and columns\n",
+                          "dim({ActiveDataSet()})"))
+}
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+#' @rdname Menu-window-functions
+#' @export
+#' @keywords internal
+command_dataset_as_df <- function() {
+    doItAndPrint(str_glue(
+        "## Change class of the dataset to `data.frame`\n",
+        "{ActiveDataSet()} <- as.data.frame({ActiveDataSet()})"))
+    command_dataset_refresh()
+}
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#' @rdname Menu-window-functions
+#' @export
+#' @keywords internal
+command_dataset_as_tibble <- function() {
+    Library("tibble")
+    doItAndPrint(str_glue(
+        "## Change class of the dataset to `tibble`\n",
+        "{ActiveDataSet()} <- as_tibble({ActiveDataSet()})"))
+    command_dataset_refresh()
+}
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#' @rdname Menu-window-functions
+#' @export
+#' @keywords internal
+command_dataset_as_dt <- function() {
+    Library("data.table")
+    doItAndPrint(str_glue(
+        "## Change class of the dataset to `data.table`\n",
+        "{ActiveDataSet()} <- data.table({ActiveDataSet()})"))
+    command_dataset_refresh()
+}
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
