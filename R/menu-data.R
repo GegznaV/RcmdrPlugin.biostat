75e983ed6dbeda1758e795b3c4cbd4349c00c045
Vilmantas Gegzna <GegznaV@gmail.com>
2018-11-22 18:58:40 +0200

    Function mooved

--- R/menu-data.R
+++ R/menu-data.R
@@ -4,27 +4,3 @@
 #' @keywords internal
 NULL
 
-# ============================================================================
-#' @rdname Menu-window-functions
-#' @export
-#' @keywords internal
-command_list_objects <- function() {
-
-    # Inputs (not implemented) -----------------------------------------------
-    show_hidden <- FALSE
-
-    if (show_hidden) {
-        all_names <- "all.names = TRUE"
-    } else {
-        all_names <- ""
-    }
-
-    # Code -------------------------------------------------------------------
-    command <-  str_glue(
-        "## List objects in R workspace \n",
-        "objects({all_names})"
-        )
-
-    doItAndPrint(command)
-
-}
--- R/menu-settings.R
+++ R/menu-settings.R
@@ -121,6 +121,32 @@ command_set_locale_default <- function() {
         Rcmdr::doItAndPrint()
 }
 
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#' @rdname Menu-window-functions
+#' @export
+#' @keywords internal
+command_list_objects <- function() {
+
+    # Inputs (not implemented) -----------------------------------------------
+    show_hidden <- FALSE
+
+    if (show_hidden) {
+        all_names <- "all.names = TRUE"
+    } else {
+        all_names <- ""
+    }
+
+    # Code -------------------------------------------------------------------
+    command <-  str_glue(
+        "## List objects in R workspace \n",
+        "objects({all_names})"
+    )
+
+    doItAndPrint(command)
+
+}
+
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 #' @rdname Menu-window-functions
 #' @export
