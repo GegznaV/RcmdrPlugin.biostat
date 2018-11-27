cc44d452e6c3c7f8a9f62dd707e340dafb15b7a3
Vilmantas Gegzna <GegznaV@gmail.com>
2018-11-27 13:56:41 +0200

    .onAttach function added

--- R/01_package.R
+++ R/01_package.R
@@ -32,3 +32,55 @@ NULL
 # }
 
 
+.onAttach <- function(libname, pkgname) {
+    if (!interactive()) {
+        return()
+    }
+
+    # Current options
+    Rcmdr_opts <- options()$Rcmdr
+
+    # If empty, convert to named list
+    if (is.null(Rcmdr_opts)) {
+        Rcmdr_opts <- list(plugins = NULL)
+    }
+
+    add_plugins <-
+        unname(grep("RcmdrPlugin.(KMggplot2|EZR.2|biostat)",
+                        installed.packages()[ , 1],
+                        value = TRUE))
+
+    # Add plugins in certain order
+    plugins <- c(setdiff(Rcmdr_opts$plugins, add_plugins),
+                 rev(sort(add_plugins)))
+
+    # plugins <- unique(c(Rcmdr_opts$plugins, pkgname))
+
+    # Open 3-window Rcmdr, if options is not defined
+    if (is.null(Rcmdr_opts$console.output)) {
+        console.output <- FALSE
+
+    } else {
+        console.output <- Rcmdr_opts$console.output
+    }
+
+    updated_opts <-
+        modifyList(Rcmdr_opts,
+                   list(plugins = plugins,
+                        console.output = console.output))
+
+    if (!identical(Rcmdr_opts, updated_opts)) {
+        # Set new options and restart R Commander
+        options(Rcmdr = updated_opts)
+
+        if ("package:Rcmdr" %in% search()) {
+            if (!getRcmdr("autoRestart")) {
+                closeCommander(ask = FALSE, ask.save = TRUE)
+                Commander()
+            }
+
+        } else {
+            Commander()
+        }
+    }
+}
