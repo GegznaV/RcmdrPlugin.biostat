5a133af3aa8db57d3562fa72b7d013bab9bacd3f
Vilmantas Gegzna <GegznaV@gmail.com>
2018-11-28 21:05:28 +0200

    Create constants.R

---
+++ R/constants.R
@@ -0,0 +1,9 @@
+bs_var_types <- c("all", "num", "fct_like", "fct", "fct2", "nfct", "chr", "lgl", NA_character_)
+
+title_var_1   <- "Variable \n(pick one)"
+title_var_n   <- "Variable \n(pick one or several)"
+title_var_0_n <- "Variable \n(pick one, several or none)"
+
+title_gr_1   <- "Groups variable \n(pick one)"
+title_gr_n   <- "Groups variable \n(pick one or several)"
+title_gr_0_n <- "Groups variable \n(pick one, several or none)"
