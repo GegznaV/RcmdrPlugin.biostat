28a77ce15d758362a628e8c429ae8d7233bef5e9
Vilmantas Gegzna <GegznaV@gmail.com>
2018-12-03 21:32:05 +0200

    Fun. dependency on 'biostat' removed

--- R/menu-models.R
+++ R/menu-models.R
@@ -3,6 +3,17 @@
 #' @rdname Menu-window-functions
 #' @export
 #' @keywords internal
+command_std_lm_coeffs <- function() {
+    Library("lm.beta")
+    doItAndPrint(str_glue(
+        "## Standardized regression coefficients\n",
+        "coef(lm.beta::lm.beta({ActiveModel()}))"
+    ))
+}
+
+#' @rdname Menu-window-functions
+#' @export
+#' @keywords internal
 command_get_model_class <- function() {
     doItAndPrint(str_glue(
         "## The class(es) of the model\n",
