9af189282ed50918bea4a04c7076ca973469c4d8
Vilmantas Gegzna <GegznaV@gmail.com>
2018-12-05 23:31:06 +0200

    Update tcltk__bs_combobox.R

--- R/tcltk__bs_combobox.R
+++ R/tcltk__bs_combobox.R
@@ -1,6 +1,7 @@
-# Title
+# TCL/TK Combo Box
 #
 # https://www.tcl.tk/man/tcl/TkCmd/ttk_combobox.htm
+# https://core.tcl.tk/bwidget/doc/bwidget/BWman/ComboBox.html#-values
 #
 # @param parent_window parent Tcl/Tk frame
 # @param variable_list
@@ -46,20 +47,20 @@ bs_combobox <- function(
     title_sticky        = "w",
     combobox_sticky     = "nw",
 
-    on_select           = function(){},
-    postcommand         = function(){},
-
-    on_click          = function() {},
-    on_double_click   = function() {},
-    on_triple_click   = function() {},
-    on_release        = function() {},
-    on_click_3        = function() {},
-    on_double_click_3 = function() {},
-    on_triple_click_3 = function() {},
-    on_release_3      = function() {},
-
+    on_select          = function(){},
+    postcommand        = function(){},
+    on_click           = function() {},
+    on_double_click    = function() {},
+    on_triple_click    = function() {},
+    on_release         = function() {},
+    on_click_3         = function() {},
+    on_double_click_3  = function() {},
+    on_triple_click_3  = function() {},
+    on_release_3       = function() {},
+    tip                = gettext_bs(
+        "For quicker search, press the \nfirst letter of variable name\non your keyboard."),
     width              = 20,
-    heihgt             = 1,
+    height             = 8,
     ...)
 {
 
@@ -71,32 +72,50 @@ bs_combobox <- function(
     tclvalue(combovar) <- initial_selection
 
     # Main -------------------------------------------------------------------
-    combobox <- ttkcombobox(
-        parent = frame,
+    combobox <- tk2combobox(
+        parent       = frame,
         values       = variable_list,
         textvariable = combovar,
         state        = state,
         export       = export,
         width        = width,
-        postcommand = postcommand,
-        ...)
+        height       = height,
+        # modifycmd    = modifycmd,
+        postcommand  = postcommand,
+        tip = tip
+        , ...
+    )
+
 
     # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-    firstChar <- tolower(substr(variable_list, 1, 1))
-
     onLetter <- function(letter) {
-        letter  <- tolower(letter)
-        current <- as.numeric(tcl(combobox, "current"))
-        current <- if (current == -1) 1 else current + 1
-        mat     <- match(letter, firstChar[-(1:current)])
-        if (is.na(mat)) return()
-        tcl(combobox, "current", current + mat - 1)
+        # Letter binding is good for read-only comboboxes only
+
+        get_first_letter <- function(str) {
+            tolower(substr(str, 1, 1))
+        }
+
+        letter   <- tolower(letter)
+        all_vals <- tk2list.get(combobox)
+        acceptable_inds <- which(get_first_letter(all_vals) %in% letter)
+
+        if (length(acceptable_inds) == 0) {
+            return()
+        }
+
+        cur_val <- tclvalue_chr(tkget(combobox))
+        cur_val_ind <- which(all_vals %in% cur_val)
+
+        if (length(cur_val_ind) == 0)
+            cur_val_ind <- 0
+
+        if (isTRUE((cur_val_ind + 1) %in% acceptable_inds)) {
+            next_ind <- cur_val_ind + 1
+        } else {
+            next_ind <- min(acceptable_inds)
+        }
+        tcl(combobox, "current", next_ind - 1)
     }
-
-    # eval_glue('on{LETTERS} <- function() onLetter{"letters"}')
-    # eval_glue('tkbind(combobox, "<{letters}>", get("on{LETTERS}"))')
-    # eval_glue('tkbind(combobox, "<{LETTERS}>", get("on{LETTERS}"))')
-
     eval_glue('tkbind(combobox, "<{letters}>", function() onLetter("{letters}"))')
     eval_glue('tkbind(combobox, "<{LETTERS}>", function() onLetter("{letters}"))')
     # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -120,6 +139,9 @@ bs_combobox <- function(
 
     result <- list(
         frame    = frame,
+        obj      = combobox,
+        var      = combovar,
+
         combobox = combobox,
         varlist  = variable_list,
         combovar = combovar)
@@ -128,11 +150,125 @@ bs_combobox <- function(
     result
 }
 
+# Helpers and methods ========================================================
+#' @rdname Helper-functions
+#' @export
+#' @keywords internal
+get_values_combobox <- function(combobox) {
+    tk2list.get(combobox)
+}
 
-# set_selection <- function(object, val) {
-#     tclvalue(object$combovar) <- val
-# }
-#
-# getSelection.combobox <- function(object){
-#     tclvalue(object$combovar)
-# }
+#' @rdname Helper-functions
+#' @export
+#' @keywords internal
+set_values_combobox <- function(combobox, values, ...) {
+    tkconfigure(combobox, values = as.character(values), ...)
+}
+
+#' @rdname Helper-functions
+#' @export
+#' @keywords internal
+get_selection_ind_combobox <- function(combobox) {
+    which(trimws(tk2list.get(combobox)) %in% tclvalue_chr(tkget(combobox)))
+}
+
+#' @rdname Helper-functions
+#' @export
+#' @keywords internal
+# Get selected values
+get_selection_combobox <- function(combobox) {
+    tclvalue_chr(tkget(combobox))
+}
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+#' @rdname Helper-functions
+#' @export
+#' @keywords internal
+get_size.combobox <- function(obj, ...) {
+    tk2list.size(obj$combobox)
+}
+
+#' @rdname Helper-functions
+#' @export
+#' @keywords internal
+get_size.tk2combobox <- function(obj, ...) {
+    tk2list.size(obj)
+}
+
+#' @rdname Helper-functions
+#' @export
+#' @keywords internal
+get_selection.combobox <- function(obj, ...) {
+    get_selection_combobox(obj$combobox)
+}
+
+
+#' @rdname Helper-functions
+#' @export
+#' @keywords internal
+set_selection.combobox <- function(obj, sel, ...) {
+    if (is.numeric(sel)) {
+        new_val <- get_values(obj)[sel]
+    } else {
+        new_val <- sel
+    }
+
+    tclvalue(obj$combovar) <- sel
+}
+
+
+#' @rdname Helper-functions
+#' @export
+#' @keywords internal
+get_values.combobox <- function(obj, ...) {
+    get_values_combobox(obj$combobox, ...)
+}
+
+#' @rdname Helper-functions
+#' @export
+#' @keywords internal
+set_values.combobox <- function(obj, values, ...) {
+    set_values_combobox(obj$combobox, values = values, ...)
+    set_selection(obj, 1)
+}
+
+
+#' @rdname Helper-functions
+#' @export
+#' @keywords internal
+tk_activate.combobox <- function(obj, ...) {
+    tk_activate(obj$combobox, ...)
+}
+
+#' @rdname Helper-functions
+#' @export
+#' @keywords internal
+tk_normalize.combobox <- function(obj, ...) {
+    tk_normalize(obj$combobox, ...)
+}
+
+#' @rdname Helper-functions
+#' @export
+#' @keywords internal
+tk_read_only.combobox <- function(obj, ...) {
+    tk_read_only(obj$combobox, ...)
+}
+
+#' @rdname Helper-functions
+#' @export
+#' @keywords internal
+tk_disable.combobox <- function(obj, ...) {
+    tk_disable(obj$combobox, ...)
+}
+
+#' @rdname Helper-functions
+#' @export
+#' @keywords internal
+tk_get_state.combobox <- function(obj) {
+    tk_get_state(obj$combobox)
+}
+
+
+
