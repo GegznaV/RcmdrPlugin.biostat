011acc0031fdf4de502f2e8d88166bf7603de2b8
Vilmantas Gegzna <GegznaV@gmail.com>
2018-12-10 01:52:33 +0200

    Update window_ws_obj_delete.R

--- R/window_ws_obj_delete.R
+++ R/window_ws_obj_delete.R
@@ -6,66 +6,70 @@
 window_data_obj_delete <- function() {
     # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     onOK <- function() {
-        obj_names <- getSelection(dataSet1Box)
+        # Cursor ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        cursor_set_busy(top)
+        on.exit(cursor_set_idle(top))
 
-        closeDialog()
+        # Get values ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        obj_names <- get_selection(var_y_box)
 
         # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-        if (length(obj_names) == 0) {
+        # Check values ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        if (object_is_not_selected(obj_names))   {return()}
+
+        obj_names_str <- str_c(obj_names, collapse = ", ")
+
+        if (length(obj_names) == 1) {
+            msg <- str_glue(
+                "Do you agree to DELETE object",
+                '\n"{obj_names_str}" permanently?')
+
+        } else {
+            msg <- str_glue(
+                "Do you agree to DELETE {length(obj_names)} following ",
+                "objects permanently: \n\n",
+                "{obj_names_str}")
+        }
+
+        option <-
+            tk_messageBox(
+                "yesno",
+                caption = "Delete objects:",
+                message = msg
+            )
+        if (option != "yes") {
             Message("No objects were deleted.", type = "note")
             return()
 
         } else {
-            obj_names_str <- str_c(obj_names, collapse = ", ")
 
-            if (length(obj_names) == 1) {
-                msg <- str_glue(
-                    "Do you want to permanently DELETE ",
-                    'the object \n"{obj_names_str}"?')
-
-            } else {
-                msg <- str_glue(
-                    "Do you want to permanently DELETE ",
-                    "{length(obj_names)} following objects: \n\n",
-                    "{obj_names_str}")
+            # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+            # Deselect active dataset if it should be deleted.
+            if (isTRUE(any(active_ds %in% obj_names))) {
+                ActiveDataSet(NULL)
             }
 
-            option <-
-                tk_messageBox(
-                    "yesno",
-                    caption = "Delete objects:",
-                    message = msg
-                )
+            # Construct the command ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+            command <-
+                str_glue(
+                    "## Delete objects \n",
+                    "remove({obj_names_str})"
+                ) %>%
+                style_cmd()
+            # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+            doItAndPrint(command)
 
-            if (option != "yes") {
-                Message("No objects were deleted.", type = "note")
-                return()
-
-            } else {
-
-                # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-                # Deselect active dataset if it should be deleted.
-                if (isTRUE(any(active_ds %in% obj_names))) {
-                    ActiveDataSet(NULL)
-                }
-
-                # Construct the command ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-                command <-
-                    str_glue(
-                        "## Delete objects \n",
-                        "remove({obj_names_str})"
-                    ) %>%
-                    style_cmd()
-                # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-                doItAndPrint(command)
-
-                Message(
-                    str_c("The object(s) were deleted: \n", obj_names_str),
-                    type = "warn")
-            }
+            Message(
+                str_c("The object(s) were deleted: \n", obj_names_str),
+                type = "warn")
         }
 
+        closeDialog()
         tkfocus(CommanderWindow())
+
+        # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        # Announce about the success to run the function `onOk()`
+        TRUE
     }
     # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
@@ -76,14 +80,14 @@ window_data_obj_delete <- function() {
     initializeDialog(title = "Delete Objects")
 
     # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-    dataSet1Box <- bs_listbox(
+    var_y_box <- bs_listbox(
         parent       = top,
         title        = "Objects to delete\n(pick one or several)",
         values       = ws_objects,
         value        = active_ds,
         selectmode   = "multiple",
         height       = 9,
-        width        = 24,
+        width        = 30,
         on_keyboard  = "scroll",
         tip  = str_c("Hold 'Ctrl' key and left-click mouse\n",
                      "to select several objects or deselect.\n",
@@ -93,11 +97,11 @@ window_data_obj_delete <- function() {
     )
 
     if (!is.null(active_ds)) {
-        tk_see(dataSet1Box, which(ws_objects %in% active_ds))
+        tk_see(var_y_box, which(ws_objects %in% active_ds))
     }
 
     # ========================================================================
-    OKCancelHelp()
+    ok_cancel_help()
 
     # Title ------------------------------------------------------------------
     fg_col <- Rcmdr::getRcmdr("title.color")
@@ -109,8 +113,8 @@ window_data_obj_delete <- function() {
         fg   = "darkred"),
         pady = c(5, 9))
     # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-    tkgrid(getFrame(dataSet1Box), sticky = "e")
+    tkgrid(getFrame(var_y_box), sticky = "e")
 
-    tkgrid(buttonsFrame, columnspan = 2)
+    tkgrid(buttonsFrame, columnspan = 2, sticky = "ew")
     dialogSuffix()
 }
