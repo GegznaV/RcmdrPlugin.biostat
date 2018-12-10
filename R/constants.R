2890518fbfbd2e976704f70d69e8985d22bd540a
Vilmantas Gegzna <GegznaV@gmail.com>
2018-12-10 21:33:29 +0200

    Update constants.R

--- R/constants.R
+++ R/constants.R
@@ -7,3 +7,21 @@ title_var_0_n <- "Variable \n(pick one, several or none)"
 title_gr_1   <- "Groups variable \n(pick one)"
 title_gr_n   <- "Groups variable \n(pick one or several)"
 title_gr_0_n <- "Groups variable \n(pick one, several or none)"
+
+
+
+tip_multiple_ctrl_letters  = str_c(
+    "Hold 'Ctrl' key and left-click mouse\n",
+    "to select several or deselect objects.\n",
+    "Use letters on keyboard for quicker \n",
+    "navigation.")
+
+tip_single_ctrl_letters  = str_c(
+    "Use letters on keyboard for quicker navigation. \n",
+    "Hold 'Ctrl' key and left-click mouse to \n",
+    "deselect an object.")
+
+# str_c(
+#     "Use letters on keyboard for quicker navigation. \n",
+#     "Hold 'Ctrl' key and left-click mouse to \n",
+#     "deselect an object.")
