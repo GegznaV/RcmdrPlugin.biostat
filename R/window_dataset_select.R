64ca98189ff8f60bd9cec3aadd5f1cfd6c1b3c95
Vilmantas Gegzna <GegznaV@gmail.com>
2018-12-10 19:01:40 +0200

    Update window_dataset_select.R

--- R/window_dataset_select.R
+++ R/window_dataset_select.R
@@ -4,7 +4,7 @@
 window_dataset_select <- function() {
 
     dataSets <- listDataSets()
-    .ds <- ActiveDataSet()
+    .ds      <- ActiveDataSet()
 
     # Functions --------------------------------------------------------------
     cmd_refresh_listbox  <- function(variables) {
@@ -29,8 +29,8 @@ window_dataset_select <- function() {
         )
 
         if (get_size(var_ds_box) == 0 || get_selection_length(var_ds_box) == 0) {
-           # Disable buttons
-           eval_glue("tk_disable({button_obj})",    eval_envir = envir)
+            # Disable buttons
+            eval_glue("tk_disable({button_obj})",    eval_envir = envir)
 
         } else {
             # Normalize buttons
@@ -69,7 +69,7 @@ window_dataset_select <- function() {
 
             body_end_section_portrait() %>%
 
-        print(doc, target = "toc_and_captions.docx")
+            print(doc, target = "toc_and_captions.docx")
 
         fs::file_show("toc_and_captions.docx")
     }
@@ -104,6 +104,159 @@ window_dataset_select <- function() {
         active_dataset(selection)
         # active_datatet(selection)
         tkfocus(CommanderWindow())
+    }
+
+    # Menu -------------------------------------------------------------------
+
+    from_clipboard <- function() {
+        closeDialog()
+        window_import_text_delim0(init_location = "clipboard")
+    }
+
+    from_package <- function() {
+        closeDialog()
+        window_import_from_pkg()
+        # window_dataset_select()
+    }
+
+    from_rds <- function() {
+        closeDialog()
+        window_import_rds()
+        # window_dataset_select()
+    }
+
+    from_rdata <- function() {
+        closeDialog()
+        window_import_rdata()
+        # window_dataset_select()
+        }
+
+    from_text_file <- function() {
+        closeDialog()
+        window_import_text_delim()
+        # window_dataset_select()
+    }
+
+    from_excel <- function() {
+        closeDialog()
+        window_import_excel()
+        # window_dataset_select()
+    }
+
+
+
+    to_rds <- function() {
+        closeDialog()
+        window_export_to_rds()
+        # window_dataset_select()
+    }
+
+    to_rdata <- function() {
+        closeDialog()
+        window_export_to_rdata()
+        # window_dataset_select()
+    }
+
+    to_r_structure <- function() {
+        .ds_1 <- get_selection(var_ds_box)
+        # closeDialog()
+
+        doItAndPrint(str_glue(
+            "## Export as R structure ('{.ds_1}')\n",
+            "dput({.ds_1})"
+        ))
+    }
+
+    to_text_file <- function() {
+        closeDialog()
+        window_export_to_textfile()
+        # window_dataset_select()
+    }
+
+    to_excel <- function() {
+        closeDialog()
+        window_export_to_excel()
+        # window_dataset_select()
+    }
+
+    to_word <- function() {
+        closeDialog()
+        # window_export_
+        # window_dataset_select()
+    }
+
+    to_pptx <- function() {
+        closeDialog()
+        # window_export_
+        # window_dataset_select()
+    }
+
+
+    # Import menus -----------------------------------------------------------
+
+    # "From clipboard..."     , 'window_import_text_delim0(init_location = "clipboard")'
+    # "From R package... "    , "window_import_from_pkg"
+    #
+    # "Import from text file (.txt, .csv, .dat, etc.)"   , "window_import_text_delim"
+    # "Import from Excel file..."                        , "window_import_excel"
+    # "Import from Rds file (.Rds, .rds)..."	           , "window_import_rds"
+    # "Import from R-data file (.RData, .Rda, .rda)..."  , "window_import_rdata"
+    # "Import from SPSS data file..."                    , "importSPSS"
+    # "Import from SAS xport file..."                    , "importSAS"
+    # "Import from SAS b7dat file..."                    , "importSASb7dat"
+    # "Import from STATA data file..."                   , "importSTATA"
+    # "Import from Minitab data file..."                 , "importMinitab"
+
+    menu_import <- function() {
+        top <- top
+
+        menu_i <- tk2menu(tk2menu(top), tearoff = FALSE)
+        menu_f <- tk2menu(menu_i, tearoff = FALSE)
+
+        tkadd(menu_i, "command", label = "From clipboard", command = from_clipboard)
+        tkadd(menu_i, "command", label = "From package",   command = from_package)
+        tkadd(menu_i, "cascade", label = "From file    " , menu = menu_f)
+
+
+        tkadd(menu_f, "command", label = "from Rds file (.Rds, .rds)..."	        , command = from_rds)
+        tkadd(menu_f, "command", label = "from R-data file (.RData, .Rda, .rda)..." , command = from_rdata)
+        tkadd(menu_f, "separator")
+        tkadd(menu_f, "command", label = "from text file (.txt, .csv, .dat, etc.)"  , command = from_text_file)
+        tkadd(menu_f, "separator")
+        tkadd(menu_f, "command", label = "from Excel file..."                       , command = from_excel)
+
+        # tkadd(menu_f, "separator")
+        # tkadd(menu_f, "command", label = "from SPSS data file..."                   , command = function() {importSPSS()})
+        # tkadd(menu_f, "command", label = "from SAS xport file..."                   , command = function() {importSAS()})
+        # tkadd(menu_f, "command", label = "from SAS b7dat file..."                   , command = function() {importSASb7dat()})
+        # tkadd(menu_f, "command", label = "from STATA data file..."                  , command = function() {importSTATA()})
+        # tkadd(menu_f, "command", label = "from Minitab data file..."                , command = function() {importMinitab()})
+
+        tkpopup(menu_i,
+                tkwinfo("pointerx", top),
+                tkwinfo("pointery", top))
+    }
+
+    # Export menus -----------------------------------------------------------
+    menu_export <- function() {
+        top <- top
+
+        menu_e <- tk2menu(tk2menu(top), tearoff = FALSE)
+
+        tkadd(menu_e, "command", label = "to Rds file (.Rds, .rds)..."	        ,   command = to_rds)
+        tkadd(menu_e, "command", label = "to R-data file (.RData, .Rda, .rda)..." , command = to_rdata)
+        tkadd(menu_e, "separator")
+        tkadd(menu_e, "command", label = "to text file (.txt, .csv, .dat, etc.)"  , command = to_text_file)
+        tkadd(menu_e, "separator")
+        tkadd(menu_e, "command", label = "to Excel file..."                       , command = to_excel)
+        # tkadd(menu_e, "command", label = "to Word table..."                       , command = to_word)
+        # tkadd(menu_e, "command", label = "to PowerPoint table..."                 , command = to_pptx)
+        tkadd(menu_e, "separator")
+        tkadd(menu_e, "command", label = "print as R structure"                   , command = to_r_structure)
+
+        tkpopup(menu_e,
+                tkwinfo("pointerx", top),
+                tkwinfo("pointery", top))
     }
 
     # Initialize -------------------------------------------------------------
@@ -255,7 +408,7 @@ window_dataset_select <- function() {
         })
 
     # tkgrid(bs_label_b(top, text = "Information about selected dataset"),
-           # pady = c(5, 0))
+    # pady = c(5, 0))
     tkgrid(i1, i2, i3, i4, i5, i6)
     tkgrid(info_buttons_frame, sticky = "e")
 
@@ -270,7 +423,6 @@ window_dataset_select <- function() {
         width =  15,
         command = function() {
             window_dataset_new_rcmdr()
-            # cmd_refresh_listbox()
             closeDialog()
 
         })
@@ -282,12 +434,7 @@ window_dataset_select <- function() {
                       "clipboard or R package.",
                       sep = "\n"),
         width =  15,
-        command = function() {
-            # closeDialog()
-            tk_messageBox("ok", "Import menu does not work yet.")
-            cmd_refresh_listbox()
-
-        })
+        command = function() {menu_import()})
 
     e0 <- tk2button(
         io_buttons_frame,
@@ -296,13 +443,7 @@ window_dataset_select <- function() {
                      sep = "\n"),
         text = "Export",
         width =  15,
-        command = function() {
-            # closeDialog()
-            tk_messageBox("ok", "Export menu does not work yet.")
-            cmd_refresh_listbox()
-
-        })
-
+        command = function() {menu_export()})
 
     tkgrid(io_buttons_frame, sticky = "e", pady = c(10, 0))
     tkgrid(c0, i0, e0)
