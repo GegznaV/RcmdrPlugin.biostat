2b65150d633e1540b437e9d02c9f6592ddee24de
Vilmantas Gegzna <GegznaV@gmail.com>
2018-12-11 03:11:07 +0200

    Update window_dataset_print_as_md.R

--- R/window_dataset_print_as_md.R
+++ R/window_dataset_print_as_md.R
@@ -38,4 +38,38 @@ window_dataset_print_as_md <- function() {
 
     doItAndPrint(command)
 }
+
+#' @rdname Menu-window-functions
+#' @export
+#' @keywords internal
+window_dataset_print_as_kable <- function() {
+
+    tbl_style   <- "simple"
+    use_caption <- TRUE
+
+    if (use_caption) {
+        caption_text <- str_glue("Dataset `{ActiveDataSet()}`")
+        tbl_caption  <- str_glue(',\n caption = "{caption_text}" ')
+
+    } else {
+        tbl_caption  <- ""
+    }
+
+
+
+    Library("tidyverse")
+    Library("knitr")
+
+
+    command <- str_glue(
+        '## Dataset as Markdown table \n',
+        # '# The dataset printed in a from that will be converted \n',
+        # '# to a table in an R Markdown report. \n',
+        '{ActiveDataSet()} %>% \n',
+        '    knitr::kable(',
+        'digits = {getOption("digits")}, format = "pandoc"{tbl_caption})') %>%
+        style_cmd()
+
+    doItAndPrint(command)
+}
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
