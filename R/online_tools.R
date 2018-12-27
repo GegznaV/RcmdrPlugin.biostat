8de8557265bd8b64b4519fac09fbb2d82cd98dc6
Vilmantas Gegzna <GegznaV@gmail.com>
2018-12-27 20:01:35 +0200

    Update online_tools.R

--- R/online_tools.R
+++ R/online_tools.R
@@ -1,11 +1,21 @@
 #' @rdname Menu-window-functions
 #' @export
 #' @keywords internal
-copy_active_ds_to_clipboard <- function() {
+export_to_clipboard_active_ds_tab <- function() {
     try(
-        clipr::write_clip(get(ActiveDataSet(), envir = .GlobalEnv)),
+        clipr::write_clip(get(ActiveDataSet(), envir = .GlobalEnv), sep = "\t"),
         silent = TRUE)
 }
+
+#' @rdname Menu-window-functions
+#' @export
+#' @keywords internal
+export_to_clipboard <- function(ds_name = ActiveDataSet(), sep = ",") {
+    try(
+        clipr::write_clip(get(ds_name, envir = .GlobalEnv), sep = sep),
+        silent = TRUE)
+}
+
 
 #' @rdname Menu-window-functions
 #' @export
