710ccfc694d9573f30768b8b2f7f47d58de377f1
Vilmantas Gegzna <GegznaV@gmail.com>
2018-12-28 17:30:35 +0200

    Update online_tools.R

--- R/online_tools.R
+++ R/online_tools.R
@@ -1,20 +1,3 @@
-#' @rdname Menu-window-functions
-#' @export
-#' @keywords internal
-export_to_clipboard_active_ds_tab <- function() {
-    try(
-        clipr::write_clip(get(ActiveDataSet(), envir = .GlobalEnv), sep = "\t"),
-        silent = TRUE)
-}
-
-#' @rdname Menu-window-functions
-#' @export
-#' @keywords internal
-export_to_clipboard <- function(ds_name = ActiveDataSet(), sep = ",") {
-    try(
-        clipr::write_clip(get(ds_name, envir = .GlobalEnv), sep = sep),
-        silent = TRUE)
-}
 
 
 #' @rdname Menu-window-functions
@@ -22,6 +5,8 @@ export_to_clipboard <- function(ds_name = ActiveDataSet(), sep = ",") {
 #' @keywords internal
 open_online_tool <- function(url = NULL, copy_to_clipboard = FALSE) {
     checkmate::assert_string(url, null.ok = TRUE)
+    checkmate::assert_logical(copy_to_clipboard)
+
     if (!pingr::is_online()) {
         open_browser <- tclvalue_chr(
             RcmdrTkmessageBox(
@@ -40,7 +25,9 @@ open_online_tool <- function(url = NULL, copy_to_clipboard = FALSE) {
     }
 
     if (choice == "yes") {
-        copy_active_ds_to_clipboard()
+        if (isTRUE(copy_to_clipboard)) {
+            export_to_clipboard_active_ds_tab()
+        }
         browseURL(url = url)
     }
 }
@@ -53,8 +40,17 @@ open_online_tool <- function(url = NULL, copy_to_clipboard = FALSE) {
 #' @export
 #' @keywords internal
 window_online_geogebra_probability  <- function() {
-    open_online_tool("https://www.geogebra.org/probability")
+    open_online_tool("https://www.geogebra.org/probability/")
 }
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#' @rdname Menu-window-functions
+#' @export
+#' @keywords internal
+window_online_image_digitizer  <- function(variables) {
+    open_online_tool("https://apps.automeris.io/wpd/")
+}
+
 
 #' @rdname Menu-window-functions
 #' @export
@@ -91,9 +87,29 @@ window_online_gpower  <- function() {
 #' @export
 #' @keywords internal
 window_online_orange  <- function() {
-    open_online_tool("https://orange.biolab.si//")
+    open_online_tool("https://orange.biolab.si/")
 }
 
+#' @rdname Menu-window-functions
+#' @export
+#' @keywords internal
+window_online_geogebra_download  <- function() {
+    open_online_tool("https://www.geogebra.org/download/")
+}
+
+#' @rdname Menu-window-functions
+#' @export
+#' @keywords internal
+window_online_past_download  <- function() {
+    open_online_tool("https://folk.uio.no/ohammer/past/")
+}
+
+#' @rdname Menu-window-functions
+#' @export
+#' @keywords internal
+window_online_bert <- function() {
+    open_online_tool("https://bert-toolkit.com/")
+}
 
 # Other ------------ ---------------------------------------------------------
 
@@ -111,3 +127,75 @@ window_online_ggplot2_ext <- function() {
     open_online_tool("http://www.ggplot2-exts.org/gallery/")
 }
 
+#' @rdname Menu-window-functions
+#' @export
+#' @keywords internal
+window_online_tidyverse <- function() {
+    open_online_tool("https://www.tidyverse.org/")
+}
+
+
+# News and tutorials ------------ ---------------------------------------------
+
+#' @rdname Menu-window-functions
+#' @export
+#' @keywords internal
+window_online_sthda  <- function() {
+    open_online_tool("http://www.sthda.com/english/")
+}
+
+#' @rdname Menu-window-functions
+#' @export
+#' @keywords internal
+window_online_bookdown <- function() {
+    open_online_tool("https://bookdown.org/")
+}
+
+
+#' @rdname Menu-window-functions
+#' @export
+#' @keywords internal
+window_online_rweekly  <- function() {
+    open_online_tool("https://rweekly.org/")
+}
+
+#' @rdname Menu-window-functions
+#' @export
+#' @keywords internal
+window_online_r_bloggers  <- function() {
+    open_online_tool("https://www.r-bloggers.com/")
+}
+
+
+#' @rdname Menu-window-functions
+#' @export
+#' @keywords internal
+window_online_r_project  <- function() {
+    open_online_tool("https://www.r-project.org/")
+}
+
+#' @rdname Menu-window-functions
+#' @export
+#' @keywords internal
+window_online_rstudio_learning  <- function() {
+    open_online_tool("https://www.rstudio.com/online-learning/")
+}
+
+
+
+
+
+#' @rdname Menu-window-functions
+#' @export
+#' @keywords internal
+window_online_stackoverflow  <- function() {
+    open_online_tool("https://stackoverflow.com/questions/tagged/r")
+}
+
+
+#' @rdname Menu-window-functions
+#' @export
+#' @keywords internal
+window_online_crossvalidated  <- function() {
+    open_online_tool("https://stats.stackexchange.com/questions/tagged/r")
+}
