e6ce0f496ea91060cbc0b2d8b028ec8bcccefa26
Vilmantas Gegzna <GegznaV@gmail.com>
2018-12-29 05:10:11 +0200

    Update bs_checkboxes.R

--- R/bs_checkboxes.R
+++ R/bs_checkboxes.R
@@ -5,63 +5,74 @@
 # Checkboxes with command functions
 # commands - a named list of commands (functions) for checkbox.
 #            The names are the same as in "boxes"
-bs_checkboxes <- defmacro(
-    window = top,
-    frame,
+bs_checkboxes <- function(
+    parent = top,
     boxes,
     initialValues = NULL,
-    labels,
-    title = NULL,
-    ttk = FALSE,
+    labels = boxes,
+    title  = NULL,
     commands = list(),
-    expr = {
+    border = FALSE,
+    tip = ""
+)
+{
+    checkmate::assert_flag(border)
 
-        # Manage `commands` ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-        if (length(commands) > 0) {
-            if (!all(names(commands) %in% boxes)) {
-                stop("`commands` must be a named list with field names: \n",
-                     paste(boxes, collapse = ", "),
-                     ".\nCurrent names: \n",
-                     paste(names(commands), collapse = ", "))
-            }
-        }
-
-        new_cmd_list <- sapply(force(boxes), function(x) function() {})
-        ..commands <- modifyList(new_cmd_list, commands)
-        # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-        ..initialValues <- if (is.null(initialValues)) rep("1", length(boxes)) else initialValues
-        assign(frame,
-               if (ttk) {
-                   tk2labelframe(
-                       window,
-                       labelwidget = tk2label(
-                           window,
-                           text = title,
-                           font = "RcmdrTitleFont",
-                           foreground = getRcmdr("title.color")))
-               } else {
-                   tkframe(window)
-               }
-        )
-        if (!is.null(title) && !ttk) {
-            tkgrid(bs_label_b(eval_text(frame),text = title), sticky = "w")
-        }
-
-        ..variables <- paste(boxes, "Variable", sep = "")
-
-        for (i in 1:length(boxes)) {
-            assign(..variables[i], tclVar(..initialValues[i]))
-            ..checkBox <- paste0(boxes[i], "CheckBox")
-
-            assign(..checkBox,
-                   ttkcheckbutton(
-                       eval(parse(text = frame)),
-                       variable = eval(parse(text = ..variables[i])),
-                       text = labels[i],
-                       command = ..commands[[i]]))
-
-            tkgrid(eval_(..checkBox), sticky = "w")
+    # Manage `commands` ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    if (length(commands) > 0) {
+        if (!all(names(commands) %in% boxes)) {
+            stop("`commands` must be a named list with field names: \n",
+                 paste(boxes, collapse = ", "),
+                 ".\nCurrent names: \n",
+                 paste(names(commands), collapse = ", "))
         }
     }
-)
+
+    boxes_list <- structure(boxes, names = boxes)
+
+    new_cmd_list <- map(boxes_list, function(x) function() {})
+    ..commands <- modifyList(new_cmd_list, commands)
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    initial_values <-
+        if (is.null(initialValues)) {
+            rep("1", length(boxes))
+
+        } else {
+            initialValues
+        }
+
+    vars <- map(initial_values, tclVar)
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    frame <-
+        if (border) {
+            tk2labelframe(
+                parent,
+                labelwidget = bs_label_b(parent, text = title))
+
+        } else {
+            tk2frame(parent)
+        }
+
+    if (!is.null(title) && !border) {
+        tkgrid(bs_label_b(frame, text = title), sticky = "w")
+    }
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    objs <- pmap(
+        list(vars, labels, ..commands),
+        ~ tk2checkbutton(frame,
+                         variable = ..1,
+                         text     = ..2,
+                         command  = ..3,
+                         tip      = tip))
+
+    walk(objs, tkgrid, sticky = "w")
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    structure(list(
+        frame = frame,
+
+        var  = structure(vars, names = boxes),
+        obj  = structure(vars, names = boxes)
+    ),
+    class = c("bs_check_boxes", "bs_tk_widget", "list"))
+}
