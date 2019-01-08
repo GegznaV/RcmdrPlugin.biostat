902d91592380b0d15e8562be454570faf8141980
Vilmantas Gegzna <GegznaV@gmail.com>
2019-01-08 04:21:42 +0200

    Create initialize_dialog.R

---
+++ R/initialize_dialog.R
@@ -0,0 +1,35 @@
+initialize_dialog <- defmacro(
+    window   = top,
+    title    = "",
+    offset   = 10,
+    preventCrisp,
+    use.tabs = FALSE,
+    notebook = notebook,
+    tabs     = c("dataTab", "optionsTab"),
+    suppress.window.resize.buttons = TRUE,
+    parent   = CommanderWindow(),
+
+    expr = {
+        if (getRcmdr("crisp.dialogs")) tclServiceMode(on = FALSE)
+        window <- tktoplevel(parent = parent, borderwidth = 10)
+
+        if (use.tabs) {
+            notebook <- ttknotebook(window)
+            for (tab in tabs) assign(tab, tkframe(window))
+        }
+
+        tkwm.title(window, title)
+
+        location <- getRcmdr("open.dialog.here")
+
+        position <- if (!is.null(location)) {
+            location
+        } else {
+            pos <- offset + commanderPosition()
+            if (any(pos < 0)) {"-50+50"} else {paste0("+", paste(pos, collapse = "+"))
+            }
+        }
+        tkwm.geometry(window, position)
+        if (suppress.window.resize.buttons) tkwm.transient(window, CommanderWindow())
+    }
+)
