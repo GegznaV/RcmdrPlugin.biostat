60d346405a81ba38d950342a5b3ea3bb367438eb
Vilmantas Gegzna <GegznaV@gmail.com>
2019-01-11 22:41:17 +0200

    Update 01_package.R

--- R/01_package.R
+++ R/01_package.R
@@ -93,18 +93,68 @@ biostat_env <- new.env()
             file = system.file("etc", file, package = package))
     }
 
+    bs_tkimage_create("::image::bs_r_logo_g"  , "icons/R-logo-g.png")
+    bs_tkimage_create("::image::bs_r_logo_g2" , "icons/R-logo-g.gif")
+    bs_tkimage_create("::image::bs_green"     , "icons/user-online-2.png")
+    bs_tkimage_create("::image::bs_green_r_24", "icons/speech-balloon-green-r-icon-24.png")
+    bs_tkimage_create("::image::bs_green_r_32", "icons/speech-balloon-green-r-icon-32.png")
+    bs_tkimage_create("::image::bs_green_r_64", "icons/speech-balloon-green-r-icon-64.png")
+    bs_tkimage_create("::image::bs_green_r_48", "icons/speech-balloon-green-r-icon-48.png")
+    bs_tkimage_create("::image::bs_green_r_72", "icons/speech-balloon-green-r-icon-72.png")
+
     bs_tkimage_create("::image::bs_dataset"  , "icons/list.png")
     bs_tkimage_create("::image::bs_model"    , "icons/model.png")
 
     bs_tkimage_create("::image::bs_locale"   , "icons/locale.png")
     bs_tkimage_create("::image::bs_down"     , "icons/down-blue.png")
     bs_tkimage_create("::image::bs_delete"   , "icons/delete.png")
-    bs_tkimage_create("::image::bs_paste"    , "icons/paste.png")
-    bs_tkimage_create("::image::bs_refresh"  , "icons/refresh.png")
-    bs_tkimage_create("::image::bs_file_open", "icons/document-open.png")
+    bs_tkimage_create("::image::bs_paste"    , "icons/edit-paste-7.png")
+    bs_tkimage_create("::image::bs_copy"     , "icons/edit-copy-7.png")
+    bs_tkimage_create("::image::bs_cut"      , "icons/edit-cut-7.png")
+    bs_tkimage_create("::image::bs_refresh"  , "icons/view-refresh-6.png")
+    bs_tkimage_create("::image::bs_open_file", "icons/document-open.png")
+    bs_tkimage_create("::image::bs_open_dir",  "icons/document-open-folder.png")
+    bs_tkimage_create("::image::bs_open",      "icons/document-open-2.png")
+
+    bs_tkimage_create("::image::bs_import",    "icons/document-import-2.png")
+    bs_tkimage_create("::image::bs_export",    "icons/document-export-4.png")
+    bs_tkimage_create("::image::bs_new",       "icons/window-new.png")
+    bs_tkimage_create("::image::bs_new_doc",   "icons/document-new-3.png")
+    bs_tkimage_create("::image::bs_to_console","icons/transform-shear-down.png")
+    bs_tkimage_create("::image::bs_preview",   "icons/document-preview.png")
+
+    # bs_tkimage_create("::image::bs_analyse",   "icons/calculator-edit.png")
+    bs_tkimage_create("::image::bs_analyse",   "icons/application-view-tile.png")
+    bs_tkimage_create("::image::bs_summary",   "icons/view-statistics.png")
+    bs_tkimage_create("::image::bs_summary2",  "icons/insert-numbers.png")
+    bs_tkimage_create("::image::bs_summary3",  "icons/kdb_table.png")
+    bs_tkimage_create("::image::bs_plot",      "icons/office-chart-pie.png")
+    bs_tkimage_create("::image::bs_plot2",     "icons/insert-chart.png")
+    bs_tkimage_create("::image::bs_plot3",     "icons/insert-chart-bar.png")
 
 }
 
+# "::image::bs_locale"
+# "::image::bs_down"
+# "::image::bs_delete"
+# "::image::bs_paste"
+# "::image::bs_copy"
+# "::image::bs_cut"
+# "::image::bs_open_file"
+# "::image::bs_open"
+#
+# "::image::bs_refresh"
+# "::image::bs_import"
+# "::image::bs_export"
+#
+# "::image::bs_new"
+#
+# "::image::bs_preview"
+# "::image::bs_plot"
+#
+# "::image::bs_summary"
+# "::image::bs_chart"
+# "::image::bs_analyse"
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 restart_commander <- function() {
