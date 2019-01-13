2d2164cf74a00c234bb446ba62d696dac29389ab
Vilmantas Gegzna <GegznaV@gmail.com>
2019-01-13 23:42:29 +0200

    Update menu-data.R

--- R/menu-data.R
+++ R/menu-data.R
@@ -14,14 +14,40 @@ NULL
 #' @export
 #' @keywords internal
 command_dataset_refresh <- function() {
-
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     .ds <- Rcmdr::ActiveDataSet()
 
-    if (!is.null(.ds)) {
-        Rcmdr::activeDataSet(.ds)
-    }
+    if (isTRUE(!.ds %in% ls(envir = .GlobalEnv))) {
+        # If ds is deleted
+        Rcmdr::ActiveDataSet(NULL)
 
+    } else if (!is.null(.ds)) {
+        # If ds is present
+        active_dataset(.ds)
+    }
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    activate_menus()
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+}
+
+#' @rdname Menu-window-functions
+#' @export
+#' @keywords internal
+activate_menus <- function() {
     Rcmdr::activateMenus()
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    # Activate buttons in BioStat mode
+    if (is_biostat_mode()) {
+
+        if (!is.null(ActiveDataSet())) {
+            tk_normalize(getRcmdr("button_export"))
+            tk_normalize(getRcmdr("button_view"))
+
+        } else {
+            tk_disable(getRcmdr("button_export"))
+            tk_disable(getRcmdr("button_view"))
+        }
+    }
 }
 
 
@@ -30,7 +56,8 @@ command_dataset_refresh <- function() {
 #' @export
 #' @keywords internal
 command_dataset_view <- function() {
-    doItAndPrint(str_glue("View({ActiveDataSet()})"))
+    justDoIt(str_glue("View({ActiveDataSet()})"))
+    logger(str_glue("## View({ActiveDataSet()})"))
 }
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
