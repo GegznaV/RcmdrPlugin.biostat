2ee27b0f1da4837e733f056d171c4e5232997850
Vilmantas Gegzna <GegznaV@gmail.com>
2019-01-14 02:27:23 +0200

    Update window_about_package.R

--- R/window_about_package.R
+++ R/window_about_package.R
@@ -7,81 +7,101 @@ window_about_package <- function() {
     # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     onOK <- function() {closeDialog(); tkfocus(CommanderWindow())}
     # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-    OKCancelHelp()
-    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     descr_file <- system.file("DESCRIPTION", package = "RcmdrPlugin.biostat")
     # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     # The main title is taken from the description file
+    upper_frame <- tkframe(top)
+    tkgrid(upper_frame)
+
     main_title_frame <-
-        labelRcmdr(top,
+        labelRcmdr(upper_frame,
                    fg = "darkblue",
-                   font = tkfont.create(family = "Times New Roman", size = 12),
+                   font = tkfont.create(family = "Times New Roman", size = 14),
                    text = desc::desc_get("Title", descr_file))
 
-    tkgrid(main_title_frame,
-           sticky = "w", pady = c(2, 15), padx = 25, columnspan = 5)
+    tkgrid(main_title_frame, pady = c(2, 15), padx = 16)
 
     tkconfigure(main_title_frame, cursor = "heart")       # Mouse cursor changes
 
     # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     info_frame <- tkframe(top)
-    tkgrid(info_frame, columnspan = 4)
+    tkgrid(info_frame)
 
     tkgrid(
-        labelRcmdr(info_frame, fg = getRcmdr("title.color"),
-                      text = gettext_bs("Package: ")),
-        labelRcmdr(info_frame, text = "RcmdrPlugin.biostat"),
-
-           sticky = "w", pady = c(2, 0), padx = 25)
+        bs_label_b(info_frame, text = gettext_bs("Package: ")),
+        bs_label(  info_frame, text = "RcmdrPlugin.biostat"),
+        sticky = "w", pady = c(2, 0), padx = 16)
 
     tkgrid(
-        labelRcmdr(info_frame,
-                   fg   = getRcmdr("title.color"),
-                   text = gettext_bs("Version: ")),
-        labelRcmdr(info_frame,
-                   text = as.character(packageVersion("RcmdrPlugin.biostat"))),
-           sticky = "w", pady = c(2, 0), padx = 25)
+        bs_label_b(info_frame, text = gettext_bs("Version: ")),
+        bs_label(info_frame,
+                 text = as.character(packageVersion("RcmdrPlugin.biostat"))),
+        sticky = "w", pady = c(2, 0), padx = 16)
 
     tkgrid(
-        labelRcmdr(info_frame,
-                   fg   = getRcmdr("title.color"),
-                   text = gettext_bs("Date: ")),
-        labelRcmdr(info_frame, text = desc::desc_get("Date", descr_file)),
-        sticky = "w", pady = c(0, 10), padx = 25
-        )
+        bs_label_b(info_frame, text = gettext_bs("Date: ")),
+        bs_label(info_frame,   text = desc::desc_get("Date", descr_file)),
+        sticky = "w", pady = c(0, 10), padx = 16
+    )
 
     # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    font_normal  <- tkfont.create(family = "Segoe UI", size = 9, underline = FALSE, weight = "normal")
+    font_over    <- tkfont.create(family = "Segoe UI", size = 9, underline = TRUE,  weight = "normal")
+
+    set_font_n <- function(W) {
+        tkconfigure(W, font = font_normal, foreground = "black")
+    }
+
+    set_font_o <- function(W) {
+        tkconfigure(W, font = font_over, foreground = "blue")
+    }
+
+
     homepage_link <- "https://gegznav.github.io/RcmdrPlugin.biostat/"
 
     on_click <- function() {browseURL(homepage_link)}
 
-    hp_frame_1 <- labelRcmdr(top, text = gettext_bs("Homepage: "))
-    hp_frame_2 <- labelRcmdr(top, fg = getRcmdr("title.color"), text = homepage_link)
+    hp_frame_1 <- bs_label_b(info_frame, text = gettext_bs("Homepage: "))
+    hp_frame_2 <- bs_label(info_frame,   text = homepage_link, font = font_normal)
 
     tkbind(hp_frame_2, "<ButtonPress-1>", on_click) # Link activation
+    tkbind(hp_frame_2, "<Enter>", set_font_o)
+    tkbind(hp_frame_2, "<Leave>", set_font_n)
+
     tkconfigure(hp_frame_2, cursor = "hand2")       # Mouse cursor changes
 
     tkgrid(hp_frame_1, hp_frame_2,
            sticky = "w",
-           padx = 25,
-           pady = c(0, 2),
-           columnspan = "3")
+           padx = 16,
+           pady = c(0, 0))
+
+    tkgrid.columnconfigure(hp_frame_2, sticky = "e")
 
     # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     issues_link <- "https://github.com/GegznaV/RcmdrPlugin.biostat/issues"
 
-    issues_frame_1 <- labelRcmdr(top, text = gettext_bs("Bug reports: "))
-    issues_frame_2 <- labelRcmdr(top, fg = getRcmdr("title.color"), text = "(link)")
+    issues_frame_1 <- bs_label_b(info_frame, text = gettext_bs("Bug reports: "))
+    issues_frame_2 <- bs_label(info_frame,   text = issues_link, font = font_normal)
 
     on_click_i <- function() {
         browseURL(issues_link)
     }
     tkbind(issues_frame_2, "<ButtonPress-1>", on_click_i) # Link is activated
-    tkconfigure(issues_frame_2, cursor = "hand2")         # Mouse cursor changes
+    tkbind(issues_frame_2, "<Enter>", set_font_o)
+    tkbind(issues_frame_2, "<Leave>", set_font_n)
 
-    tkgrid(issues_frame_1, issues_frame_2, pady = c(0, 10), columnspan = "3")
+    tkconfigure(issues_frame_2, cursor = "hand2")        # Mouse cursor changes
 
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-    tkgrid(buttonsFrame, columnspan = "5", sticky = "")
+    tkgrid(issues_frame_1, issues_frame_2,  # pady = c(0, 10), columnspan = "3",
+           sticky = "w",
+           padx = 16,
+           pady = c(0, 10))
+
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    ok_cancel_help()
+
+    tkgrid(buttonsFrame)
+    tkgrid.remove(cancelButton)
+
     dialogSuffix()
 }
