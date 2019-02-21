8df6265da457bd6860ef94e051eee0fa478b6d7d
Vilmantas Gegzna <GegznaV@gmail.com>
2019-02-22 00:44:02 +0200

    Update window_summary_variables.R

--- R/window_summary_variables.R
+++ R/window_summary_variables.R
@@ -1,3 +1,7 @@
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#' @rdname Menu-window-functions
+#' @export
+#' @keywords internal
 window_summary_variables <- function() {
     function_not_implemented()
 }
