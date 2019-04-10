83c3518f67ec7eae1acfbd53ed982eaebd0b87d9
Vilmantas Gegzna <GegznaV@gmail.com>
2019-04-10 21:46:11 +0300

    Update window_import_from_text.R

--- R/window_import_from_text.R
+++ R/window_import_from_text.R
@@ -3,6 +3,7 @@
 #  1. Add context menus with (clear, paste, clear and paste, cut, copy)
 #  2. Enable more than one NA string.
 #  3. Add warnig, if file name changed, and contents did not.
+#  4. "tkxview.moveto" etc. may not be working. "after" is needed
 
 
 #' @rdname Menu-window-functions
