f2e3b5ff47c574aa9e7da3c644c1ed6717a85529
Vilmantas Gegzna <GegznaV@gmail.com>
2019-05-08 02:44:46 +0300

    Window for "mutate" redesigned (not completed)

--- R/window_variable_mutate.R
+++ R/window_variable_mutate.R
@@ -1,12 +1,19 @@
 # TODO:
 #
-# 1. When push "Apply" button and an error occurs, two windows open.
-#    "Apply" is now disabled and this should be fixed.
+# [+] 1. When push "Apply" button and an error occurs, two windows open.
+#        "Apply" is now disabled and this should be fixed.
 #
 # 2. In variable box text "[factor]" should be differentiated
 #    to "[character]", "[logical]", "[factor]"
 #
 # 3. Add buttons "+", "-", "*", etc. in style as used in "fit linear model" window
+#
+# 4. Add examples. May be button for pop-up window with examples
+#
+# 5. Enable computations by group
+#
+# 6. on apply sucess:  View and select the new variable [???]
+
 
 
 #' @rdname Menu-window-functions
@@ -20,24 +27,23 @@ window_variable_mutate0  <- function(variables) {
 #' @rdname Menu-window-functions
 #' @export
 #' @keywords internal
-# var_name (character) - variable name
-# expr (character) - expression to be computed
-# incorrect_expr_msg (character) - Message for incorrect expression.
-window_variable_mutate <- function(var_name = NULL,
-                                   init_express = NULL,
-                                   incorrect_expr_msg = NULL) {
+window_variable_mutate <- function() {
 
     # Functions --------------------------------------------------------------
-    onDoubleClick_variable <- function() {
-        var <- trim.blanks(getSelection(variablesBox))
+    insert_selected_variable <- function() {
+        # var <- trim.blanks(getSelection(variablesBox))
+        var <- get_selection(f1_vars$y)
 
         word <- str_glue('\\[{gettext_bs("factor")}\\]')
 
         if (length(grep(word, var)) == 1)
             var <- trim.blanks(sub(word, "", var))
-        tkfocus(compute)
-        expr <- trim.blanks(tclvalue(computeVar))
-        tclvalue(computeVar) <-
+
+        tkfocus(f2_entry_expr$obj_text)
+
+        expr <- get_values(f2_entry_expr)
+
+        new_expr <-
             if (expr == "") {
                 var
             } else {
@@ -45,85 +51,25 @@ window_variable_mutate <- function(var_name = NULL,
                 expr_sep <- if (last_chr %in% c("(", "[")) "" else " "
                 paste(expr, var, sep = expr_sep)
             }
-        tkicursor(compute, "end")
-        tkxview.moveto(compute, "1")
+
+        set_values(f2_entry_expr, new_expr)
+
+        tkicursor(f2_entry_expr$obj_text,      "end")
+        tkxview.moveto(f2_entry_expr$obj_text, "1")
     }
 
-    # Dialog -----------------------------------------------------------------
-    .ds <- active_dataset()
 
-    initializeDialog(
-        title = gettext_bs("Mutate: create new or replace existing variable"))
-
-    .variables <- Variables()
-    variables <-
-        paste(.variables,
-              ifelse(.variables %in% Factors(),
-                     yes = gettext_bs("[factor]"),
-                     no  = ""
-              ))
-
-    var_box_Frame <- tkframe(top)
-
-    variablesBox <-
-        variableListBox2(
-            var_box_Frame,
-            variables,
-            title = gettext_bs("Current variables \n(double-click to add to expression)"),
-            listHeight = 8,
-            onDoubleClick_fun = onDoubleClick_variable
-        )
-
-    # tkbind(variablesBox$listbox, "<Double-ButtonPress-1>", onDoubleClick_variable)
-
-    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-    computeFrame <- tkframe(top)
-
-    if (is.null(var_name))
-        var_name <- unique_colnames_2(gettext_bs("new_variable"))
-
-    newVariableName <- tclVar(var_name)
-
-    newVariable <-
-        ttkentry(computeFrame,
-                 width = "20",
-                 textvariable = newVariableName)
-
-    expression_Frame <- tkframe(computeFrame)
-
-    if (is.null(init_express)) init_express <- ""
-
-    computeVar <- tclVar(init_express)
-    compute <-
-        ttkentry(
-            expression_Frame,
-            font = getRcmdr("logFont"),
-            width = "50",
-            textvariable = computeVar
-        )
-    computeXscroll <- ttkscrollbar(
-        expression_Frame,
-        orient = "horizontal",
-        command = function(...)
-            tkxview(compute, ...)
-    )
-    tkconfigure(
-        compute,
-        xscrollcommand = function(...)
-            tkset(computeXscroll, ...)
-    )
-
-    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    # onOK -------------------------------------------------------------------
     onOK <- function() {
         # Cursor ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
         cursor_set_busy(top)
         on.exit(cursor_set_idle(top))
 
         # Get values ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-        new_name  <- trim.blanks(tclvalue(newVariableName))
-        express <- tclvalue(computeVar)
-
-
+        new_name   <- get_values(f2_name_entry)
+        express    <- get_values(f2_entry_expr)
+        use_groups <- get_values(f1_vars$checkbox)
+        group_vars <- get_values(f1_vars$gr)
 
         # Check if expression is not empty ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
@@ -187,7 +133,6 @@ window_variable_mutate <- function(var_name = NULL,
             # Close dialog ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
             # closeDialog()
 
-
         } else {
             logger_error(command, error_msg = result)
             show_code_evaluation_error_message(parent = top)
@@ -205,71 +150,113 @@ window_variable_mutate <- function(var_name = NULL,
         # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     }
 
+
+
+    # Dialog -----------------------------------------------------------------
+    .ds <- active_dataset()
+
+    initializeDialog(
+        title = gettext_bs("Mutate: create new or replace existing variable"))
+
+
+
+    get_labeled_variables <- function(variables) {
+
+        vars <- Variables()
+        paste(vars,
+              ifelse(vars %in% Factors(),
+                     yes = gettext_bs("[factor]"),
+                     no  = ""
+              ))
+    }
+
+
+    f1 <- tkframe(top)
+
+    f1_vars <-
+        bs_listbox_y_gr(
+            parent      = f1,
+            y_title     = gettext_bs("Current variables \n(double-click to add to expression)"),
+            list_height = 8,
+            y_vars      = get_labeled_variables(),
+            y_params    = list(on_double_click = insert_selected_variable),
+            ch_label    = "Compute by group"
+        )
+
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    f2 <- tkframe(top)
+
+    f2_name_entry <- bs_entry(
+        parent = f2,
+        width = "20",
+        label = "New variable name",
+        label_position = "above",
+        value = unique_colnames_2("new_variable")
+    )
+
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    f2_entry_expr <- bs_entry(
+        f2,
+        label = "Expression to compute",
+        label_position = "above",
+        width = 62,
+        value = "",
+        scroll_x = TRUE)
+
+    # Grid -------------------------------------------------------------------
     # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     ok_cancel_help(
         close_on_ok = TRUE,
         helpSubject = "mutate", helpPackage = "dplyr",
         reset = "window_variable_mutate",
-        apply = "window_variable_mutate"
+        apply = "window_variable_mutate",
+        after_apply_success_fun = function() {
+            set_values(f2_name_entry, unique_colnames_2("new_variable"))
+            set_values(f1_vars$y, get_labeled_variables())
+
+            # View and select the new variable [???]
+        }
     )
-
-    tkgrid(var_box_Frame, sticky = "nw")
-
-    examples_Frame <- tkframe(var_box_Frame)
-
-    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-    tkgrid_text <- function(text = "", frame = examples_Frame, fg = "black",
-                            sticky = "w", padx = 20, pady = 0, ...) {
-        tkgrid(labelRcmdr(frame, text = gettext_bs(text), fg = fg),
-               sticky = sticky, padx = padx, pady = pady, ...)
-    }
-    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-    tkgrid_text("\nExamples of expressions", fg = getRcmdr("title.color"))
-    tkgrid_text('Polular operations:   +   -   *   /   ^   sqrt()    log()   rank()', fg = "darkgreen")
-
-    tkgrid_text("Example 1: log(age)")
-    tkgrid_text("Example 2: a + b")
-    tkgrid_text("Example 3: as.factor(color)")
-    tkgrid_text("Example 4: weight / (height^2)")
-    tkgrid_text('Example 5: ifelse(age < 50, "young", "old")')
-
-    if (!is.null(incorrect_expr_msg)) {
-        tkgrid_text(incorrect_expr_msg, fg = "darkred", sticky = "e",
-                    pady = c(5, 0))
-    }
     # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-    tkgrid(getFrame(variablesBox), examples_Frame,
+    # examples_Frame <- tkframe(f1)
+    #
+    # # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    # tkgrid_text <- function(text = "", frame = examples_Frame, fg = "black",
+    #                         sticky = "w", padx = 20, pady = 0, ...) {
+    #     tkgrid(labelRcmdr(frame, text = gettext_bs(text), fg = fg),
+    #            sticky = sticky, padx = padx, pady = pady, ...)
+    # }
+    #
+    # # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    # tkgrid_text("\nExamples of expressions", fg = getRcmdr("title.color"))
+    # tkgrid_text('Polular operations:   +   -   *   /   ^   sqrt()    log()   rank()', fg = "darkgreen")
+    #
+    # tkgrid_text("Example 1: log(age)")
+    # tkgrid_text("Example 2: a + b")
+    # tkgrid_text("Example 3: as.factor(color)")
+    # tkgrid_text("Example 4: weight / (height^2)")
+    # tkgrid_text('Example 5: ifelse(age < 50, "young", "old")')
+
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    tkgrid(f1_vars$frame,  # examples_Frame,
            sticky = "nw",
            columnspan = 2)
 
     # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-    tkgrid(
-        labelRcmdr(computeFrame,
-                   fg = getRcmdr("title.color"),
-                   text = gettext_bs("New variable name")),
-        labelRcmdr(computeFrame, text = "   "),
-        labelRcmdr(computeFrame,
-                   fg = getRcmdr("title.color"),
-                   text = gettext_bs("Expression to compute")),
-        pady = c(15, 0),
-        sticky = "nw")
+    tkgrid(f1, sticky = "nw")
+    tkgrid(f2, sticky = "nw")
 
-    tkgrid(newVariable,
-           labelRcmdr(computeFrame, text = " = "),
-           expression_Frame,
-           sticky = "nw")
+    f2_eq <- bs_label(f2, text = " = ")
 
-    tkgrid(compute, sticky = "ew")
-    tkgrid(computeXscroll, sticky = "ew")
+    tkgrid(f2_name_entry$frame, f2_eq, f2_entry_expr$frame,
+           pady = c(15, 0),
+           sticky = "new")
 
-    # tkgrid(variablesFrame, sticky = "nw")
-    tkgrid(computeFrame, sticky = "nw")
-
+    tkgrid.configure(f2_eq, sticky = "ns")
 
     # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     tkgrid(buttonsFrame, sticky = "ew", columnspan = 2)
-    dialogSuffix(rows = 3,
-                 columns = 2,
-                 focus = compute)
+    dialogSuffix(rows = 3, columns = 2, focus = f2_entry_expr$obj_text)
 }
+
