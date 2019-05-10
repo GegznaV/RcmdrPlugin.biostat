090cc18b7dad061965c09e37b172dd0349430ea1
Vilmantas Gegzna <GegznaV@gmail.com>
2019-05-11 01:59:20 +0300

    Move to top/end fixed

--- R/window_variable_recode.R
+++ R/window_variable_recode.R
@@ -95,14 +95,17 @@ right_click_menu <- function(tcl_widget) {
             text <- tclvalue(textVar)
             putRcmdr("last.search", text)
             if (text == "") {
-                errorCondition(recall=onFind, message=gettext_bs("No search text specified."))
+                errorCondition(
+                    recall = onFind,
+                    message = gettext_bs("No search text specified."))
                 return()
             }
             type <- if (tclvalue(regexprVariable) == 1) "-regexp" else "-exact"
             case <- tclvalue(caseVariable) == 1
             direction <- tclvalue(directionVariable)
             stop <- if (direction == "-forward") "end" else "1.0"
-            where.txt <- if (case) tksearch(focused, type, direction, "--", text, "insert", stop)
+            where.txt <-
+                if (case) tksearch(focused, type, direction, "--", text, "insert", stop)
             else tksearch(focused, type, direction, "-nocase", "--", text, "insert", stop)
             where.txt <- tclvalue(where.txt)
             if (where.txt == "") {
@@ -125,13 +128,13 @@ right_click_menu <- function(tcl_widget) {
             return("")
         }
         OKCancelHelp()
-        tkgrid(labelRcmdr(textFrame, text=gettext_bs("Search for:")),
-               textEntry, sticky="w")
-        tkgrid(textFrame, sticky="w")
-        tkgrid(optionsFrame, sticky="w")
-        tkgrid(directionFrame, sticky="w")
-        tkgrid(buttonsFrame, sticky="w")
-        dialogSuffix(focus=textEntry)
+        tkgrid(labelRcmdr(textFrame, text = gettext_bs("Search for:")),
+               textEntry, sticky = "w")
+        tkgrid(textFrame, sticky = "w")
+        tkgrid(optionsFrame, sticky = "w")
+        tkgrid(directionFrame, sticky = "w")
+        tkgrid(buttonsFrame, sticky = "w")
+        dialogSuffix(focus = textEntry)
     }
     # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     onSelectAll <- function() {
@@ -250,10 +253,7 @@ right_click_menu <- function(tcl_widget) {
 
 
     tkbind(tcl_widget, "<ButtonPress-3>", crete_context_menu)
-
 }
-
-
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # Helper functions
@@ -324,6 +324,16 @@ get_cursor_position <- function(recodes) {
         c("row", "col")
     )
 }
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+get_selection_ind <- function(recodes) {
+
+    selection <- tclvalue(tktag.ranges(recodes, "sel"))
+
+    setNames(
+        as.integer(str_split_fixed(selection, "[. ]", n = 4)),
+        c("from_row", "from_col", "to_row", "to_col")
+    )
+}
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 select_line <- function(recodes, i) {
@@ -331,9 +341,55 @@ select_line <- function(recodes, i) {
     tktag.add(recodes, "sel", str_glue("{i}.0"), str_glue("{i}.end"))
 }
 
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+move_elements <- function(x, ind, move_to) {
+    #       x - vector;
+    #     ind - selected rows;
+    # move_to - "top", "-1", "+1", "end" - place to move rows to.
+
+    move_to <- match.arg(move_to, choices = c("top", "-1", "+1", "end"))
+
+    switch(
+        move_to,
+        # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        "top" = {
+            x <- c(x[ind], x[-ind])
+        },
+
+        # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        "-1" = {
+            if ((min(ind) - 1) < 1)
+                return(x)
+
+            for (i in sort(ind)) {
+                x <- swap(x, i, i - 1)
+            }
+        },
+
+        # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        "+1" = {
+            n <- length(x)
+
+            if ((max(ind) + 1) > n)
+                return(x)
+
+            for (i in sort(ind, decreasing = TRUE)) {
+                x <- swap(x, i, i + 1)
+            }
+        },
+
+        # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        "end"  = {
+            x <- c(x[-ind], x[ind])
+        }
+    )
+    x
+}
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-move_selected_row_in_tktext <- function(recodes, k = "+1") {
+move_selected_row_in_tktext <- function(recodes, move_to = "+1") {
+
+    move_to <- match.arg(move_to, choices = c("top", "-1", "+1", "end"))
 
     # selection <- tclvalue(tktag.ranges(recodes, "sel"))
 
@@ -348,14 +404,19 @@ move_selected_row_in_tktext <- function(recodes, k = "+1") {
     recodes_text <- str_remove(tclvalue(tkget(recodes, "1.0", "end")), "\\n$")
     tmp <- structure(str_split(recodes_text, "\n")[[1]], class = "glue")
 
-    n <- length(tmp)
+    # n <- length(tmp)
+    #
+    # j <- get_j(move_to, i, n)
+    #
+    # i <- correct_row_index(i, n)
+    # j <- correct_row_index(j, n)
+    #
+    #
+    # new_text <- swap(tmp, i, j)
 
-    j <- get_j(k, i, n)
+    new_text <- move_elements(x = tmp, ind = i, move_to = move_to)
 
-    i <- correct_row_index(i, n)
-    j <- correct_row_index(j, n)
-
-    swapped <- structure(str_c(swap(tmp, i, j), collapse = "\n"), class = "glue")
+    swapped <- structure(str_c(new_text, collapse = "\n"), class = "glue")
 
     # str_count(recodes_text, "\n")
     # str_count(swapped, "\n")
@@ -363,35 +424,22 @@ move_selected_row_in_tktext <- function(recodes, k = "+1") {
     tkdelete(recodes, "1.0", "end")
     tkinsert(recodes, "1.0", swapped)
 
+    # [???] adapt for multiple selected lines
+    j <- get_j(move_to, i, length(tmp))
+
     pos_j <- str_glue("{j}.0")
 
     tkmark.set(recodes, "insert", pos_j)
 
     tclAfter(1, function() {
-        tksee(recodes, pos_j)
-        select_line(recodes, j)
         tkfocus(recodes)
+        select_line(recodes, j)
+        tksee(recodes, pos_j)
     })
 
     # # Suppress default bindings
     # tcl("expr", "{break}")
     #
-}
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-# widget - tktext widget
-# i, j - row numbers
-swap_rows_in_tktext <- function(widget, i, j) {
-
-    recodes_text <- trimws(tclvalue(tkget(widget, "1.0", "end")))
-    tmp <- str_split(recodes_text, "\n")[[1]]
-    n <- length(tmp)
-    i <- correct_row_index(i, n)
-    j <- correct_row_index(j, n)
-
-    swapped <- str_c(swap(tmp, i, j), collapse = "\n")
-
-    tkdelete(widget, "1.0", "end")
-    tkinsert(widget, "1.0", swapped)
 }
 
 # ___ Main function ___ ======================================================
@@ -530,7 +578,7 @@ window_variable_recode0 <- function() {
         f1_but_set_1,
         image = "::image::bs_go_top",
         command = function() {
-            move_selected_row_in_tktext(recodes, "top")
+            move_selected_row_in_tktext(recodes, move_to = "top")
         },
         tip = "Move selected line \nto the top."
     )
@@ -538,7 +586,7 @@ window_variable_recode0 <- function() {
         f1_but_set_1,
         image = "::image::bs_go_up",
         command = function() {
-            move_selected_row_in_tktext(recodes, "-1")
+            move_selected_row_in_tktext(recodes, move_to = "-1")
         },
         tip = "Move selected line \nup by 1 position."
     )
@@ -546,7 +594,7 @@ window_variable_recode0 <- function() {
         f1_but_set_1,
         image = "::image::bs_go_down",
         command = function() {
-            move_selected_row_in_tktext(recodes, "+1")
+            move_selected_row_in_tktext(recodes, move_to = "+1")
         },
         tip = "Move selected line \ndown by 1 position."
     )
@@ -554,7 +602,7 @@ window_variable_recode0 <- function() {
         f1_but_set_1,
         image = "::image::bs_go_bottom",
         command = function() {
-            move_selected_row_in_tktext(recodes, "bottom")
+            move_selected_row_in_tktext(recodes, move_to = "end")
         },
         tip = "Move selected line \nto the bottom."
     )
@@ -743,9 +791,10 @@ window_variable_recode0 <- function() {
     )
     # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-    ok_cancel_help(helpSubject = "recode_factor", helpPackage = "dplyr",
-                   reset = "window_variable_recode0()",
-                   apply = "window_variable_recode0()")
+    ok_cancel_help(
+        helpSubject = "recode_factor", helpPackage = "dplyr",
+        reset = "window_variable_recode0()",
+        apply = "window_variable_recode0()")
 
     tkgrid(f1, sticky = "nw")
 
@@ -779,8 +828,9 @@ window_variable_recode0 <- function() {
     tkgrid(
         bs_label(
             recodesFrame,
-            text = gettext_bs(
-                "Enter recode directives\n(one directive per row; change order of rows, if needed)"),
+            text = gettext_bs(str_c(
+                "Enter recode directives\n",
+                "(one directive per row; change order of rows, if needed)")),
             # "Enter recode directives\n(one directive per row or comma separated)"),
             fg = getRcmdr("title.color"),
             font = "RcmdrTitleFont"
@@ -834,3 +884,70 @@ window_variable_recode0 <- function() {
     # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     dialogSuffix(bindReturn = FALSE)
 }
+
+# ============================================================================
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+move_selected_row_in_tktext_old <- function(recodes, k = "+1") {
+
+    # selection <- tclvalue(tktag.ranges(recodes, "sel"))
+
+    # if (selection == "") {
+    #     select_line(recodes, i)
+    #     return()
+    # }
+
+    i <- get_cursor_position(recodes)["row"]
+    # select_line(recodes, i)
+
+    recodes_text <- str_remove(tclvalue(tkget(recodes, "1.0", "end")), "\\n$")
+    tmp <- structure(str_split(recodes_text, "\n")[[1]], class = "glue")
+
+    n <- length(tmp)
+
+    j <- get_j(k, i, n)
+
+    i <- correct_row_index(i, n)
+    j <- correct_row_index(j, n)
+
+    swapped <-
+        structure(str_c(swap(tmp, i, j), collapse = "\n"), class = "glue")
+
+    # str_count(recodes_text, "\n")
+    # str_count(swapped, "\n")
+
+    tkdelete(recodes, "1.0", "end")
+    tkinsert(recodes, "1.0", swapped)
+
+    pos_j <- str_glue("{j}.0")
+
+    tkmark.set(recodes, "insert", pos_j)
+
+    tclAfter(1, function() {
+        tkfocus(recodes)
+        select_line(recodes, j)
+        tksee(recodes, pos_j)
+    })
+
+    # # Suppress default bindings
+    # tcl("expr", "{break}")
+    #
+}
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# widget - tktext widget
+# i, j - row numbers
+swap_rows_in_tktext <- function(widget, i, j) {
+
+    recodes_text <- trimws(tclvalue(tkget(widget, "1.0", "end")))
+    tmp <- str_split(recodes_text, "\n")[[1]]
+    n <- length(tmp)
+    i <- correct_row_index(i, n)
+    j <- correct_row_index(j, n)
+
+    swapped <- str_c(swap(tmp, i, j), collapse = "\n")
+
+    tkdelete(widget, "1.0", "end")
+    tkinsert(widget, "1.0", swapped)
+}
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
