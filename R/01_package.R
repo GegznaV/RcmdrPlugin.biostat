ac7cc4ea7c81c9f4c685ad66894348a030f54a81
Vilmantas Gegzna <GegznaV@gmail.com>
2019-08-19 01:43:57 +0300

    Update 01_package.R

--- R/01_package.R
+++ R/01_package.R
@@ -120,8 +120,8 @@ biostat_env$use_relative_path <- TRUE
     bs_tkimage_create("::image::bs_model"     , "icons/model.png")
     bs_tkimage_create("::image::bs_locale"    , "icons/locale.png")
 
-    bs_tkimage_create("::image::bs_ok",        "icons/dialog-ok-apply-5.png")
-    bs_tkimage_create("::image::bs_tick",      "icons/dialog-ok-apply-5.png")
+    bs_tkimage_create("::image::bs_ok"        , "icons/dialog-ok-apply-5.png")
+    bs_tkimage_create("::image::bs_tick"      , "icons/dialog-ok-apply-5.png")
     bs_tkimage_create("::image::bs_delete"    , "icons/delete.png")
     bs_tkimage_create("::image::bs_paste"     , "icons/edit-paste-7.png")
     bs_tkimage_create("::image::bs_copy"      , "icons/edit-copy-7.png")
