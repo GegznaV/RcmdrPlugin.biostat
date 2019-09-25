fcffc9a02d6735b3ecf27d0374873b9ef96d9506
Vilmantas Gegzna <GegznaV@gmail.com>
2019-09-25 15:42:21 +0300

    File moved

--- docs/inst/check-pkgs.R
+++ docs/inst/check-pkgs.R
@@ -5,6 +5,7 @@ cat("")
 cat(sep = "\n", '
 \n# [Copy this code and paste into Console]
 
+# A dalis
 old_opts <-
   options(
     repos = "https://cran.rstudio.com/",
@@ -13,13 +14,18 @@ old_opts <-
     install.packages.compile.from.source = "always"
   )
 
+# B dalis
 install.packages("rvg")
 install.packages("officer")
+install.packages("Rcmdr")
+install.packages("RcmdrPlugin.KMggplot2")
 
+# C dalis
 if (!require("remotes"))  install.packages("remotes")
-remotes::install_github("GegznaV/RcmdrPlugin.biostat")
+remotes::install_github("GegznaV/RcmdrPlugin.biostat")  # reikiama versija 0.0.43
 remotes::install_version("RcmdrPlugin.KMggplot2", version = "0.2-5")
 remotes::install_version("Rcmdr", version = "2.5-3")
 
+# D dalis
 options(old_opts)
 ')
