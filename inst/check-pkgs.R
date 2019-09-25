f3ae8a90a7a4ae1d79d5100bfe25da0c9912bd74
Vilmantas Gegzna <GegznaV@gmail.com>
2019-09-25 04:07:10 +0300

    Update check-pkgs.R

--- inst/check-pkgs.R
+++ inst/check-pkgs.R
@@ -1,2 +1,25 @@
 # No code yet
-cat("\n[This feature does not work yet!]\n")
+cat("")
+
+
+cat(sep = "\n", '
+\n# [Copy this code and paste into Console]
+
+old_opts <-
+  options(
+    repos = "https://cran.rstudio.com/",
+    pkgType = "both",
+    install.packages.check.source = "yes",
+    install.packages.compile.from.source = "always"
+  )
+
+install.packages("rvg")
+install.packages("officer")
+
+if (!require("remotes"))  install.packages("remotes")
+remotes::install_github("GegznaV/RcmdrPlugin.biostat")
+remotes::install_version("RcmdrPlugin.KMggplot2", version = "0.2-5")
+remotes::install_version("Rcmdr", version = "2.5-3")
+
+options(old_opts)
+')
