31f56e0ba8fbcb696278b1672d7b896d14c80695
Vilmantas Gegzna <GegznaV@gmail.com>
2020-02-21 17:49:48 +0200

    Addin was simplified.

--- R/RStudio-addins.R
+++ R/RStudio-addins.R
@@ -1,26 +1,8 @@
 load_rcmdr_biostat <- function() {
 
-  all_pkgs <- .packages(all.available = TRUE)
-
-  pkg1 <-
-    if ("RcmdrPlugin.KMggplot2" %in% all_pkgs) {
-      "library(RcmdrPlugin.KMggplot2)\n"
-    } else {
-      ""
-    }
-
-  # pkg2 <-
-  #   if ("RcmdrPlugin.EZR.as.menu" %in% all_pkgs) {
-  #     "library(RcmdrPlugin.EZR.as.menu)\n"
-  #   } else {
-  #     ""
-  #   }
-
   command <-
     paste0(
       "library(Rcmdr) \n",
-      pkg1,
-      # pkg2,
       "library(RcmdrPlugin.biostat) \n",
       "load_rcmdr_biostat_mode()"
     )
