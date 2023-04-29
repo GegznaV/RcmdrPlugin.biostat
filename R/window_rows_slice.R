6c4cb5db483de68e0c098de8597000c01ba9dcfc
Vilmantas Gėgžna <vilmantas.gegzna@gmc.vu.lt>
2023-04-29 15:29:03 +0300

    Use `inherits()` instead of `class()`

--- R/window_rows_slice.R
+++ R/window_rows_slice.R
@@ -67,9 +67,8 @@ window_rows_slice <- function() {
     # indexRows <- paste0("c(", gsub(" ", ",", index), ")")
     # index <- try(str_glue_eval(indexRows), silent = TRUE)
 
-    if (class(index) == "try-error") {
-      errorCondition(recall = window_rows_slice,
-        message = index)
+    if (inherits(index, "try-error")) {
+      errorCondition(recall = window_rows_slice, message = index)
       closeDialog()
       return()
     }
