97fec0a306ae1cf2daebfe3e3ab57dad8873608d
Vilmantas Gėgžna <GegznaV@gmail.com>
2026-03-01 02:27:28 +0200

    Avoid magrittr dependencies

--- R/01_package.R
+++ R/01_package.R
@@ -16,7 +16,6 @@
 #' @import nortest
 #' @import ggplot2
 #' @importFrom dplyr glimpse is_grouped_df
-#' @importFrom magrittr "%>%"  "%<>%"  "%T>%"  "%$%"
 #' @importFrom graphics plot par
 #' @importFrom stats p.adjust.methods C setNames
 #' @importFrom utils browseURL packageVersion data installed.packages modifyList
